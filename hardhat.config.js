df11be6b50e02e77de5d245d892bca634b555f99
Luiz <luizhamilton29@gmail.com>
2021-04-03 20:06:03 -0300

    First version

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+
+module.exports = {
+  solidity: "0.8.3",
+};
